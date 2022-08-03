69acfff9069baa1f2b8da5ce7d768b13254e4fd9
anil <anil_baniya@outlook.com>
2022-08-03 14:16:51 +0545

    link added

--- strStr.php
+++ strStr.php
@@ -1,4 +1,8 @@
 <?php
+// What should we return when needle is an empty string? This is a great question to ask during an interview.
+
+// For the purpose of this problem, we will return 0 when needle is an empty string. This is consistent to C's strstr() and Java's indexOf().
+// https://gist.github.com/wszdwp/c3cd212f449ef9cb289d
 
 function strStrImplementation($haystack, $needle) {
     if(strlen($needle) == 0) return 0;
