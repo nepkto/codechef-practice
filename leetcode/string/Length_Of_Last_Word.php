8f54d1fea8af708c78882b6b072b85ba283fc836
anil <anil_baniya@outlook.com>
2022-08-05 16:32:11 +0545

    lenght of last word

--- leetcode/string/Length_Of_Last_Word.php
+++ leetcode/string/Length_Of_Last_Word.php
@@ -1,26 +1,56 @@
 <?php
+function lengthOfLastWord($str)
+{
+    // iteration from last approach
+    $count = 0;
+    $flag = false;
+    for ($i = strlen($str) - 1; $i >= 0; $i--) {
+        // Once the first character from last
+        // is encountered, set char_flag to true.
+        if (($str[$i] >= 'a' && $str[$i] <= 'z') ||
+            ($str[$i] >= 'A' && $str[$i] <= 'Z')
+        ) {
+            $flag = true;
+            $count++;
+        }
 
-function lengthOfLastWord($s) {
+        // When the first space after the
+        // characters (from the last) is
+        // encountered, return the length
+        // of the last word
+        else {
+            if ($flag == true)
+                $count;
+                break;
+        }
+        // echo $count;
+    }
+    echo $count . "<br>";
+}
+
+lengthOfLastWord("   fly me   to   the moon  ");
+lengthOfLastWord("a");
+lengthOfLastWord("Hello World");
+
+function lengthOfLastWord1($s)
+{
     $arr = str_split(trim($s));
     $str = '';
-    for($i = count($arr); $i >= 0; $i--) {
-        if($arr[$i] == ' ') {
+    for ($i = count($arr); $i >= 0; $i--) {
+        if ($arr[$i] == ' ') {
             break;
         } else {
             $str .= $arr[$i];
         }
     }
-    
+
     return strlen($str);
 }
-lengthOfLastWord("   fly me   to   the moon  ");
-lengthOfLastWord("a");
-lengthOfLastWord("Hello World");
 
 
-function lengthOfLastWord2($s) {
-    $arr = explode(" ",trim($s));
-    $lastWord = $arr[count($arr) -1];
+function lengthOfLastWord2($s)
+{
+    $arr = explode(" ", trim($s));
+    $lastWord = $arr[count($arr) - 1];
     return strlen($lastWord);
-        
 }
